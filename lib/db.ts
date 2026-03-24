68d14444169fba7829aa3a8f43cec202846d7155
Tomás D. Vera Molano <tomasv0127@gmail.com>
2026-03-23 20:59:18 -0500

    feat: creación de base de datos

---
+++ lib/db.ts
@@ -0,0 +1,24 @@
+import path from 'node:path'
+import { PrismaClient } from './generated/prisma'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
+
+function createPrismaClient() {
+  const client = createClient({
+    url: `file:${path.join(process.cwd(), 'data', 'lab.db')}`,
+  })
+  const adapter = new PrismaLibSQL(client)
+  return new PrismaClient({ adapter })
+}
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const db = globalForPrisma.prisma ?? createPrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = db
+}
+
+export default db
